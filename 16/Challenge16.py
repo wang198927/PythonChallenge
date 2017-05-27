941aa72b16ab518b388b599040c8a1cb49c0c073
wang198927 <wang198927@163.com>
2017-05-27 22:19:19 +0800

    16

--- 16/Challenge16.py
+++ 16/Challenge16.py
@@ -1,1 +1,10 @@
 #test
+from PIL import Image
+im = Image.open(r'mozart.gif')
+newim = Image.new('RGB',(640,480))
+for i in range(480):
+    for j in range(640):
+        if im.getpixel((j,i)) == 195:
+            [newim.putpixel((k,i),im.getpixel(( (k+j)%640,i))) for k in range(640)]
+            break
+newim.save(r'out16.gif')
